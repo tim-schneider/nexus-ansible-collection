ea5488656cfc39f473a969945442ddd1ac7739f9
Brian Veltman <2551674+brianveltman@users.noreply.github.com>
2025-03-11 21:04:04 +0100

    chore: add encoding and metadata to scripts
    Added shebang and UTF-8 encoding declaration to both the main module and test script. Updated copyright information with a new email address.

--- tests/unit/plugins/modules/test_download.py
+++ tests/unit/plugins/modules/test_download.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright: (c) 2025, Brian Veltman <info@cloudkrafter.org>
+# GNU General Public License v3.0+ (see https://www.gnu.org/licenses/gpl-3.0.txt)
+
+
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
